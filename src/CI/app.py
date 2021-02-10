71567af551e3a95fe69f3b4f3a56e18d7a7fd656
Caroline Borg <cawibo@gmail.com>
2021-02-10 19:34:06 +0100

    Fix #27 Refactor app.py (#28)
    * Fix #27 Refactor app.py
    
    * small fix in github logic
    
    Co-authored-by: Caroline Borg <caroline.borg@valtech.com>
    Co-authored-by: JakobBerggren <jaberggr@kth.se>

--- src/CI/app.py
+++ src/CI/app.py
@@ -7,7 +7,7 @@ from pylint import epylint as lint
 import requests
 import notification
 
-auth_token = os.environ['AUTH_TOKEN']
+auth_token = '5b206135ed78930e1d8516619bb641734ea5229c'
 
 app = Flask(__name__)
 
@@ -25,6 +25,67 @@ def github_webhook_handler():
         handle_push(payload)
     return ""
 
+def parse_pylint(string):
+    """Parses the pylint stdout string returns the result.
+        returns None if no output found."""
+    lines = string.split('\n')
+
+    for line in lines[::-1]:
+        result = parse.parse(' Your code has been rated at {:F}/10 (previous run: {:F}/10, +{:F})', line)
+        if result is None:
+            continue
+
+        return result[0]
+
+def parse_pytest(string):
+    """Parses the pytest output string and returns passed and failed test counts.
+    If no counts are found, None is returned instead for that case.
+    """
+    lines = string.split('\n')
+
+    for line in lines[::-1]:
+        if not parse.parse('========================={}=========================', line):
+            continue
+        passed = parse.search('{:d} passed', line)
+        failed = parse.search('{:d} failed', line)
+        errors = parse.search('errors', line)
+        if passed:
+            passed = passed[0]
+        if failed:
+            failed = failed[0]
+        if errors:
+            errors = True
+
+        return passed, failed, errors
+
+    return None, None
+
+def clone_repo(payload, target_dir):
+    """Clones the repo declared in 'payload' into 'target_dir'"""
+    os.system("git clone {} {} --depth=1 --no-single-branch".format(payload["repository"]["clone_url"], target_dir))
+    os.system("git -C {} pull".format(target_dir))
+    os.system("git -C {} checkout {}".format(target_dir, payload["after"]))
+
+def remove_repo(target_dir):
+    """Removes 'target_dir' dir from file system"""
+    os.system("rm -rf {}".format(target_dir))
+
+def exec_pylint(target_dir):
+    """Executes pylint on 'target_dir'"""
+    (pylint_stdout, pylint_stderr) = lint.py_run(target_dir, return_std=True)
+    pylint_output = pylint_stdout.read()
+    return pylint_output
+
+def exec_pytest(target_dir):
+    """Executes pyte4st on 'target_dir'"""
+    pytest_output = subprocess.run(["python3","-m","pytest"],text=True,capture_output=True,cwd=target_dir).stdout
+    return pytest_output
+
+def send_email(payload, target_dir, pylint_output, pytest_output):
+    """Sends an email with payload-, pylint-, and pytest content"""
+    subject = '[{}] {} "{}"'.format(payload["repository"]["full_name"], target_dir, payload["commits"][0]["message"])
+    notification.send_notification('Subject: {}\n\n{}'.format(subject, str(pylint_output) + "\n" + str(pytest_output)))
+
 def handle_push(payload):
     repo_id = payload["repository"]["id"]
     commit_sha = payload["after"]
@@ -32,26 +93,17 @@ def handle_push(payload):
 
     update_status(payload,"pending")
 
-    os.system("git clone {} {}".format(payload["repository"]["clone_url"],repo_directory)) 
-   
-    #Switch to branch
-    os.system("git -C {} pull".format(repo_directory))
-    os.system("git -C {} checkout {}".format(repo_directory,commit_sha))
+    clone_repo(payload, repo_directory)
 
+    pylint_output = exec_pylint(repo_directory)
+    pytest_output = exec_pytest(repo_directory)
 
-    #Run pylint
-    (pylint_stdout, pylint_stderr) = lint.py_run(repo_directory, return_std=True)
-    pylint_output = pylint_stdout.read()
+    send_email(payload, repo_directory, pylint_output, pytest_output)
+
+    remove_repo(repo_directory)
+
     print(pylint_output)
-
-    #Run pytest
-    pytest_output = subprocess.run(["python3","-m","pytest"],text=True,capture_output=True,cwd=repo_directory).stdout
     print(pytest_output)
-
-    subject = '[{}] {} "{}"'.format(payload["repository"]["full_name"], repo_directory, payload["commits"][0]["message"])
-    notification.send_notification('Subject: {}\n\n{}'.format(subject, pylint_output + "\n" + pytest_output))
-
-    os.system("rm -rf {}".format(repo_directory))
 
     if "ERRORS" in pytest_output:
         update_status(payload,"error","The commit testing resulted in some errors")
@@ -61,9 +113,7 @@ def handle_push(payload):
         update_status(payload,"success","The commit testing failed")
 
 
-def update_status(payload,status="success",description="CI"):
-
-
+def update_status(payload, status="success", description="CI"):
     repo_name = payload["repository"]["full_name"]
     sha = payload["after"]
     url = 'https://api.github.com/repos/{repo_name}/statuses/{sha}'.format(
@@ -84,9 +134,9 @@ def update_status(payload,status="success",description="CI"):
     requests.post(url, data=json, headers=headers)
 
 if __name__ == '__main__':
-    with open("data/demo_payload.json") as f:
+    with open("src/CI/data/demo_payload.json") as f:
         payload = json.load(f)
 
-    handle_push(payload)
+        handle_push(payload)
     app.run(debug=True, host='0.0.0.0',port=80)
     
