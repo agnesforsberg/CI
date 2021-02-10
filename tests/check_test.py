04326f08b35be63a8e45828ed7b477d0cc831f74
Caroline <cawibo@gmail.com>
2021-02-10 22:23:42 +0100

    feat #32 : Test for update_status.

---
+++ tests/check_test.py
@@ -0,0 +1,34 @@
+import unittest
+import requests
+from src.CI import app
+
+
+class TestCheckCommit(unittest.TestCase):
+    def test_check(self):
+        with open("/tmp/auth") as f:
+            auth = f.read().strip()
+
+            sha = "976fd7f5f2024665c5363cab2b35b4efc0ea3d53"
+            payload = {
+                "repository": {
+                    "full_name": "agnesforsberg/CI"
+                },
+                "after": sha
+            }
+
+            headers = {
+                'Authorization': 'Bearer ' + auth
+            }
+            get_url = "https://api.github.com/repos/agnesforsberg/CI/commits/{sha}/status".format(sha=sha)
+            
+            app.update_status(payload, status="success")
+            res = requests.get(get_url, headers=headers)
+            ctx = res.json()
+            self.assertEqual(ctx['state'], "success")
+
+            app.update_status(payload, status="pending")
+            res = requests.get(get_url, headers=headers)
+            ctx = res.json()
+            self.assertEquals(ctx['state'], "pending")
+
+        
